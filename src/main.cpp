40dcce8c34a95ce4c16b2beb5cbd954490ab7965
Ethan Schweinsberg <ees37@zips.uakron.edu>
2017-04-24 20:08:43 -0400

    *Temperature sensor code is working

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 #include <Arduino.h>
+#include "OneWire.h"
+#include "DallasTemperature.h"
+
+#define ONE_WIRE_PIN 11
+
+/* start a one wire instance on the desired pin */
+OneWire oneWire(ONE_WIRE_PIN);
+
+/* initialize the temperature library using one wire */
+DallasTemperature tempSensor(&oneWire);
 
 void setup( void )
 {
+  /* init console logging */
+  Serial.begin(9600);
+  while(!Serial);
 
+  /* init the temperature sensor */
+  tempSensor.begin();
 }
 
 void loop( void )
 {
-  
+  tempSensor.requestTemperatures();
+  Serial.println(tempSensor.getTempCByIndex(0));
+
+  delay(2000);
 }
